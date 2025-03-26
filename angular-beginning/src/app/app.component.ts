ce240b5e5608e40f9060483dca09e82ff8268759
Mohd Danish <dkhan895@gmail.com>
2025-03-26 16:28:25 +0530

    Added login and signup components and counter application

--- angular-beginning/src/app/app.component.ts
+++ angular-beginning/src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  x= 10;
+  y:number = 20;
+
   title = 'angular-beginning';
   namefromappcomponent = 'properties from angular ts file';
+  mufunctionvar: string = "";
+  counterStarttingValue:number = 0;
+  myFunction(){
+    this.mufunctionvar = "my function is called";
+  }
+  decrementCounter(){
+    if (this.counterStarttingValue>0){
+      this.counterStarttingValue = this.counterStarttingValue -1;
+    }
+  }
+
+  resetCounter(){
+    this.counterStarttingValue = 0;
+  }
+
+  incrementCounter(){
+    this.counterStarttingValue++;
+  }
+
+
 }
