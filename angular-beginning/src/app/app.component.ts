1bbba67fc94263e56765877429cab9035d8a433d
Mohd Danish <dkhan895@gmail.com>
2025-04-02 00:39:19 +0530

    added to do , two way binding,signals, directives and routing

--- angular-beginning/src/app/app.component.ts
+++ angular-beginning/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, effect, WritableSignal } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './signup/signup.component'; 
+import { signal,Signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ LoginComponent, SignupComponent],
+  imports: [ LoginComponent, SignupComponent, FormsModule, NgFor, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,6 +23,57 @@ export class AppComponent {
   myName:string= "";
   myFinalName:string= "";
   email:string="";
+  toggleelseifboolean = false;
+
+  users = ["Danish", "Afifa"];
+  
+  count = signal(10);
+  dataWritable:WritableSignal<number|string> = signal(33);
+  dataComputable:Signal<number> = computed(()=>66);
+  
+  computedX = signal(33);
+  computedY = signal(33);
+  normalZ = this.x + this.y;
+  computedZ = computed(()=>this.computedX() + this.computedY());
+
+  myTwoWayBindingVariable:string ="";
+  myTwoWayBindingVariable2:string ="";
+
+  task:string = ""
+  taskList: {id:number , task:string }[] = [];
+
+  constructor() {
+    effect(() =>{
+      console.log(this.count());
+    })
+  }
+
+  deleteTask(taskid: number){
+    this.taskList = this.taskList.filter(item => item.id != taskid);
+
+  }
+  addTask(){
+    this.taskList.push({id: this.taskList.length + 1, task : this.task})
+  }
+  nameChange(event:Event){
+    this.myTwoWayBindingVariable2 = (event.target as HTMLInputElement).value;
+  }
+  updatedOnChangesNormally(){
+    this.x = 40;
+  }
+  updatedOnChangesSignal(){
+    this.computedX.set(400);
+  }
+  updateSignalValue(){
+    //this.count.set(30);
+    this.count.set(100);
+    //this.dataComputable.set(3333); error since this is read only
+  }
+
+  
+  toggleifelse(){
+    this.toggleelseifboolean=!this.toggleelseifboolean;
+  }
   myFunction(){
     this.mufunctionvar = "my function is called";
   }
